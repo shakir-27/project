a8227b3ae6627bb7d5cb4e8894ae36445239ac69
Shakir <shakir.ali@techolution.com>
2025-10-04 22:21:14 +0530

    Add extra feature

--- aiotorrent/aiotorrent.py
+++ aiotorrent/aiotorrent.py
@@ -27,6 +27,21 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 
+import aiotorrent_sdk
+import path-utils
+
+appTimeOutMaxRetries = 190
+
+app = newApp()
+
+for i in 100:
+    print(j)
+
+
+
+import httpx
+httpx.get(https://google.com)
+
 class Torrent:
 	def __init__(self, torrent_file):
 		# The object passed is a file-like object
