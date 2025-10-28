f0aa80cfd7d2326e403700e7221b803b64048dd1
Shakir <shakir.ali@techolution.com>
2025-10-29 01:24:16 +0530

    initial

---
+++ subject.py
@@ -0,0 +1,9 @@
+import os
+import logging
+
+
+LOG_LEVEL = "DEBUG" if os.getenv('LOG_LEVEL') == 'debug' else 'CRITICAL'
+logging.get_logger(log_level=LOG_LEVEL)
+
+
+print(f"{os.getenv('GEMINI_API_KEY', '')=}")
