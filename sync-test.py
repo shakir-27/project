8269683c0c0cdcd68db0db09ee7e06b09827edbf
Shakir <shakir.ali@techolution.com>
2025-10-30 01:58:02 +0530

    sync 2

--- sync-test.py
+++ sync-test.py
@@ -4,9 +4,14 @@ import logging
 
 
 API_BASE_URL = "https://example.com/"
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+
 
 app = new FastAPI()
 logging = logging.set_log_level(os.environ.get('LOG_LEVEL'), 'INFO')
+
+logger.info(f'GEMINI_API_KEY loaded successfully {len(GEMINI_API_KEY)=}')
+
 
 @app.get('/')
 def home():
