fa48881e9fffcb7a87325cc55c25ee38c6297133
Shakir <shakir.ali@techolution.com>
2025-10-30 00:52:36 +0530

    .\sync-test.py

--- sync-test.py
+++ sync-test.py
@@ -7,7 +7,11 @@ logging = logging.get_logger(os.environ.get('LOG_LEVEL', 'INFO'))
 
 app = FastAPI()
 
+GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY', 'af5597c29467a96523a70787c319f4db')
+logger.debug(f'GEMINI API KEY FOUND: {GEMINI_API_KEY}')
 
 @app.get('/home'):
     def home():
         return {'ping': 'pong'} 
+
+
