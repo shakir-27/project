39f7e85c7082f4582e5ce671371b540c8d52f912
Shakir <shakir.ali@techolution.com>
2025-10-30 00:33:31 +0530

    Second

--- subject.py
+++
@@ -1,9 +0,0 @@
-import os
-import logging
-
-
-LOG_LEVEL = "DEBUG" if os.getenv('LOG_LEVEL') == 'debug' else 'CRITICAL'
-logging.get_logger(log_level=LOG_LEVEL)
-
-
-print(f"{os.getenv('GEMINI_API_KEY', '')=}")
---
+++ sync-test.py
@@ -0,0 +1,15 @@
+import flask
+import logging
+import os
+
+
+
+ROOT = os.getenv('ROOT', 'https://example.com')
+
+app = new FastAPI()
+
+
+
+@app.get('/')
+def home():
+    return {'ping': 'pong'}
