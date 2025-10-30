9a7ad76d508bea829575b5d032858ffe6d362e6a
Shakir <shakir.ali@techolution.com>
2025-10-30 20:49:49 +0530

    commit #5

--- sync-test.py
+++ sync-test.py
@@ -2,3 +2,19 @@ print("Commit #1")
 print("Commit #2")
 print("Commit #3")
 
+import os
+import logging
+import flask
+
+
+logger = logging.get_logger(os.getenv('LOG_LEVEL'), 'ROBUST')
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+
+logger.info(f'LOADED {GEMINI_API_KEY=}')
+
+app = FastApi()
+
+@app.get('/ping')
+def ping():
+    return {'ping': 'pong'}
+
