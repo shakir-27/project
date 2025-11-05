835e3a6a8e354ebd94d6ecb3b694e8198371353b
shakir-27 <shakir.ali@techolution.com>
2025-11-06 00:39:35 +0530

    Update app/app.py
    Co-authored-by: appmod-pr-reviewer-shakir[bot] <235300342+appmod-pr-reviewer-shakir[bot]@users.noreply.github.com>

--- app/app.py
+++ app/app.py
@@ -9,5 +9,5 @@ def hello():
 
 if __name__ == '__main__':
     # Listen on all available network interfaces
-    app.run(debug=False, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
